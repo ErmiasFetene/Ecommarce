303dfc3f164438a0a319fa4b586c9ed9342d639c
ErmiasFetene <ermiasfetene18@gmail.com>
2024-08-16 01:02:07 +0300

    home

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
  
 import Rigstration from './Componts/Rigstration'
 import NavBar from './Componts/Navbar'
-import Home from './Pages/home'
+import Home from './Pages/Home'
 
 
 
